6bd7539435c7aa69a3b1b46ff720ebd340cca397
Fernando Pimenta <108306295+fsoupimenta@users.noreply.github.com>
2023-01-09 19:19:55 -0300

    Criação da página generica

--- src/App.js
+++ src/App.js
@@ -1,12 +1,7 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import './App.css'
 import MenuLateral from './componentes/MenuLateral';
-import Paciente from './paginas/Paciente';
-import Dentista from './paginas/Dentista'
-import Recepcionista from './paginas/Recepcionista'
-import Procedimento from './paginas/Procedimento'
-import Especialidade from './paginas/Especialidade'
-import Convenio from './paginas/Convenio'
+import PaginaGenerica from './paginas/PaginaGenerica';
 
 function App() {
 	return (
@@ -14,12 +9,13 @@ function App() {
 			<div className="container">
 				<MenuLateral />
 				<Routes>
-					<Route path='/paciente' element={<Paciente />}/>
-					<Route path='/dentista' element={<Dentista />}/>
-					<Route path='/recepcionista' element={<Recepcionista />}/>
-					<Route path='/procedimento' element={<Procedimento />}/>
-					<Route path='/especialidade' element={<Especialidade />} />
-					<Route path='/convenio' element={<Convenio />} />
+					<Route path='/' element={<PaginaGenerica />}/>
+					<Route path='/paciente' element={<PaginaGenerica titulo="Paciente" />}/>
+					<Route path='/dentista' element={<PaginaGenerica titulo="Dentista" />}/>
+					<Route path='/recepcionista' element={<PaginaGenerica titulo="Recepcionista" />}/>
+					<Route path='/procedimento' element={<PaginaGenerica titulo="Procedimento" />}/>
+					<Route path='/especialidade' element={<PaginaGenerica titulo="Especialidade" />} />
+					<Route path='/convenio' element={<PaginaGenerica titulo="Convenio" />} />
 				</Routes>
 			</div>
 		</BrowserRouter>
--- src/paginas/PaginaGenerica.jsx
+++ src/paginas/PaginaGenerica.jsx
@@ -1,12 +1,12 @@
 import React from "react";
 import TelaInicialGenerica from "../componentes/TelaInicialGenerica";
 
-const Dentista = () => {
+const PaginaGenerica = ({titulo}) => {
     const colunas =[
         {
             nome: "Nome", classe:"borda-lateral", sort: true
         },
-        {
+        {   
             nome: "E-mail", classe:"borda-lateral", sort: true
         },
         {
@@ -15,23 +15,23 @@ const Dentista = () => {
     ]
     const linhas =[
         {
-            coluna1: "Dentista Sobrenome 1", coluna2: "d1@gmail.com", coluna3: "32991608320"  
+            coluna1: "Paciente Sobrenome 1", coluna2: "p1@gmail.com", coluna3: "32991608320"  
         },
         {
-            coluna1: "Dentista Sobrenome 2", coluna2: "d2@gmail.com", coluna3: "32991608320"  
+            coluna1: "Paciente Sobrenome 2", coluna2: "p2@gmail.com", coluna3: "32991608320"  
         },
         {
-            coluna1: "Dentista Sobrenome 3", coluna2: "d3@gmail.com", coluna3: "32991608320"  
+            coluna1: "Paciente Sobrenome 3", coluna2: "p3@gmail.com", coluna3: "32991608320"  
         },
     ]
     return(
         <div className="conteudo-principal">
             <div className='cabeçalho-principal' >
-                <h1>Dentista</h1>
+                <h1>{titulo}</h1>
             </div>
-            <TelaInicialGenerica titulo={'Dentista'} colunas={colunas} linhas={linhas} />
+            <TelaInicialGenerica titulo={titulo} colunas={colunas} linhas={linhas} />
         </div>
     )
 }
 
-export default Dentista
+export default PaginaGenerica
