cc4e634632220ab986707cc131e2ad29dbb0c227
Fernando Pimenta <108306295+fsoupimenta@users.noreply.github.com>
2023-01-09 20:10:32 -0300

    Inserção de dados dinâmicos para tabelas

--- src/App.js
+++ src/App.js
@@ -3,6 +3,9 @@ import './App.css'
 import MenuLateral from './componentes/MenuLateral';
 import Agenda from './paginas/Agenda'
 import PaginaGenerica from './paginas/PaginaGenerica';
+import {colunaPessoa, colunaEspecialidade, linhaPaciente, linhaDentista,
+linhaConvenio, linhaRecepcionista, linhaProcedimento, linhaEspecialidade} 
+from './data/tabela_info';
 
 function App() {
 	return (
@@ -11,13 +14,19 @@ function App() {
 				<MenuLateral />
 				<Routes>
 					<Route path='/' element={<PaginaGenerica />}/>
-					<Route path='/paciente' element={<PaginaGenerica titulo="Paciente" />}/>
-					<Route path='/dentista' element={<PaginaGenerica titulo="Dentista" />}/>
-					<Route path='/recepcionista' element={<PaginaGenerica titulo="Recepcionista" />}/>
-					<Route path='/procedimento' element={<PaginaGenerica titulo="Procedimento" />}/>
-					<Route path='/especialidade' element={<PaginaGenerica titulo="Especialidade" />} />
-					<Route path='/convenio' element={<PaginaGenerica titulo="Convenio" />} />
-          <Route path='/agenda' element={<Agenda />} />
+					<Route path='/paciente' element={<PaginaGenerica 
+					titulo="Paciente" coluna={colunaPessoa} linha={linhaPaciente} />}/>
+					<Route path='/dentista' element={<PaginaGenerica 
+					titulo="Dentista" coluna={colunaPessoa} linha={linhaDentista}  />}/>
+					<Route path='/recepcionista' element={<PaginaGenerica 
+					titulo="Recepcionista" coluna={colunaPessoa} linha={linhaRecepcionista}  />}/>
+					<Route path='/procedimento' element={<PaginaGenerica 
+					titulo="Procedimento" coluna={colunaEspecialidade} linha={linhaProcedimento} />}/>
+					<Route path='/especialidade' element={<PaginaGenerica 
+					titulo="Especialidade" coluna={colunaEspecialidade} linha={linhaEspecialidade} />} />
+					<Route path='/convenio' element={<PaginaGenerica 
+					titulo="Convenio" coluna={colunaPessoa} linha={linhaConvenio} />} />
+          			<Route path='/agenda' element={<Agenda />} />
 				</Routes>
 			</div>
 		</BrowserRouter>
