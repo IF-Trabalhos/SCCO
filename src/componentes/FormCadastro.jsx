fc9aaadd529580dd9591a60122c90d9c0fb131a6
Igor Rosa <igorrosafilgueiras@gmail.com>
2023-01-15 18:34:54 -0300

    Cadastro design

--- src/componentes/FormCadastro.jsx
+++ src/componentes/FormCadastro.jsx
@@ -3,9 +3,7 @@ import './FormCadastro.css'
 const FormCadastro = ({ labelDinamica }) => {
   console.log(labelDinamica)
   return (
-    <div className='container-cadastro'>
-
-
+    <div className='container-form-cadastro '>
       <div className='bloco'>
         <h3>IDENTIFICAÇÃO</h3>
 
@@ -28,51 +26,37 @@ const FormCadastro = ({ labelDinamica }) => {
 
         </div>
       </div>
-      <div className='endereco'>
+      <div className='bloco'>
         <h3>ENDEREÇO</h3>
-      <div className='linha1-endereco'>
-        
           <label htmlFor="inputCep">CEP:<input type="number" name='inputCep' className='cep' required /></label>
 
           <label htmlFor="inputUf">UF:<input type="text" name='inputUf' className='uf' required /></label>
 
-     
+
           <label htmlFor="inputCidade">Cidade:<input type="text" name='inputCidade' className='cidade' required /></label>
 
-      </div>
-      <div className='linha2-endereco'>
-          <label htmlFor="inputLogradouro">Logradouro:<input type="text" name='inputLogradouro'className='logradouro' required /></label>
+          <label htmlFor="inputLogradouro">Logradouro:<input type="text" name='inputLogradouro' className='logradouro' required /></label>
 
-      
-    
+
+
           <label htmlFor="inputNumero">Numero:<input type="text" name='inputNumero' className='numero' required /></label>
-
-      </div>
-      <div className='linha3-endereco'>
           <label htmlFor="inputBairro">Bairro:<input type="text" name='inputBairro' className='bairro' required /></label>
-          
-        
 
-        
+
+
+
           <label htmlFor="inputComplemento">Complemento:<input type="text" name='inputComplemento' className='complemento' required /></label>
 
-     </div>   
-    </div>
-      <div className='contato'>
-        <h3>CONTATO</h3>
-        <div>       
+      </div>
+      <div className='bloco'>
+          <h3>CONTATO</h3>
           <label htmlFor="inputEmail">E-mail:<input type="text" required name='inputEmail' /></label>
-
-  
           <label htmlFor="inputCelular">Celular:<input type="text" required name='inputCelular' /></label>
-
-   
-      
           <label htmlFor="inputTelefone">Telefone:<input type="text" required name='inputTelefone' /></label>
-
-        </div>
       </div>
     </div>
+
+
   )
 }
 
