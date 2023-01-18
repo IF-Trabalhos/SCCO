7907a139fdb496dc011a3c0fffaf0d3316790d35
Fernando Pimenta <108306295+fsoupimenta@users.noreply.github.com>
2023-01-18 12:06:24 -0300

    Implementação do axios.get

--- src/App.js
+++ src/App.js
@@ -1,14 +1,39 @@
+import React, {useEffect, useState} from 'react';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import './App.css'
 import MenuLateral from './componentes/MenuLateral';
 import Agenda from './paginas/Agenda'
 import PaginaGenerica from './paginas/PaginaGenerica';
-import {colunaPessoa, colunaEspecialidade, linhaPaciente, linhaDentista,
-linhaConvenio, linhaRecepcionista, linhaProcedimento, linhaEspecialidade} 
+import {colunaPessoa, colunaEspecialidade} 
 from './data/tabela_info';
+import axios from 'axios';
+import { BASE_URL } from './config/axios';
 
 function App() {
-	const dados = require('./data/db.json')
+	const [pessoa, setPessoa] = useState([]);
+	const [convenio, setConvenio] = useState([]);
+	const [especialidade, setEspecialidade] = useState([]);
+	const [procedimento, setProcedimento] = useState([]);
+
+	async function buscar() {
+		await axios.get(`${BASE_URL}/pessoas`).then((response) => {
+			setPessoa(response.data);
+		})
+		await axios.get(`${BASE_URL}/convenios`).then((response) => {
+			setConvenio(response.data);
+		})
+		await axios.get(`${BASE_URL}/especialidades`).then((response) => {
+			setEspecialidade(response.data);
+		})
+		await axios.get(`${BASE_URL}/procedimentos`).then((response) => {
+			setProcedimento(response.data);
+		})
+	}
+
+	useEffect(() => {
+		buscar(); // eslint-disable-next-line
+	}, []);
+
 	return (
 		<BrowserRouter>
 			<div className="container">
@@ -16,17 +41,17 @@ function App() {
 				<Routes>
 					<Route path='/' element={<PaginaGenerica />}/>
 					<Route path='/paciente' element={<PaginaGenerica 
-					titulo="Paciente" coluna={colunaPessoa} linha={dados.pessoas} />}/>
+					titulo="Paciente" coluna={colunaPessoa} linha={pessoa} />}/>
 					<Route path='/dentista' element={<PaginaGenerica 
-					titulo="Dentista" coluna={colunaPessoa} linha={dados.pessoas}  />}/>
+					titulo="Dentista" coluna={colunaPessoa} linha={pessoa}  />}/>
 					<Route path='/recepcionista' element={<PaginaGenerica 
-					titulo="Recepcionista" coluna={colunaPessoa} linha={dados.pessoas} />}/>
+					titulo="Recepcionista" coluna={colunaPessoa} linha={pessoa} />}/>
 					<Route path='/procedimento' element={<PaginaGenerica 
-					titulo="Procedimento" coluna={colunaEspecialidade} linha={dados.procedimentos} />}/>
+					titulo="Procedimento" coluna={colunaEspecialidade} linha={procedimento} />}/>
 					<Route path='/especialidade' element={<PaginaGenerica 
-					titulo="Especialidade" coluna={colunaEspecialidade} linha={dados.especialidades} />} />
+					titulo="Especialidade" coluna={colunaEspecialidade} linha={especialidade} />} />
 					<Route path='/convenio' element={<PaginaGenerica 
-					titulo="Convenio" coluna={colunaPessoa} linha={dados.convenios} />} />
+					titulo="Convenio" coluna={colunaPessoa} linha={convenio} />} />
           			<Route path='/agenda' element={<Agenda />} />
 				</Routes>
 			</div>
