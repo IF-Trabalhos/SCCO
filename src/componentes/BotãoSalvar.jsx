72c700b67ddedb6d28c3362bd5c83b16bc1e1e03
Giulianobrjf <giuliano.leite2002@gmail.com>
2023-01-28 18:05:37 -0300

    Responsividade do botão ajustada

--- src/componentes/BotãoSalvar.jsx
+++ src/componentes/BotãoSalvar.jsx
@@ -4,10 +4,10 @@ import './BotaoSalvar.css'
 const BotãoSalvar = ({setBotaoFalse, infos}) => {
     return (
         <div className='container-botao'>
-            <button className='botaoSalvar' type="button" id="botao-excluir" onClick={() => {
+            <button className='botao' type="button" id="botao-excluir" onClick={() => {
                 setBotaoFalse(false)
             }} >Excluir</button>
-            <button className='botaoSalvar' type="button" id="botao-salvar" onClick={() => {
+            <button className='botao' type="button" id="botao-salvar" onClick={() => {
                 setBotaoFalse(false)
             }}>Salvar</button>
         </div>
